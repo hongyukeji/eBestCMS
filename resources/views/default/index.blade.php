ae9afd01962f73bc47e9f8dfb2064757e20830b1
Shadow <admin@hongyuvip.com>
2018-03-04 22:33:52 +0800

    eBestCMS

--- resources/views/default/index.blade.php
+++ resources/views/default/index.blade.php
@@ -73,7 +73,7 @@
             <div class="navbar-header">
                 <!-- Mobile Toggle Menu Button -->
                 <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i></i></a>
-                <a class="navbar-brand" href="http://www.hongyuvip.com">鸿宇科技</a>
+                <a class="navbar-brand" href="http://www.hongyuvip.com">{{ config('app.name', 'eBestCMS') }}</a>
             </div>
             <div id="navbar" class="navbar-collapse collapse">
                 <ul class="nav navbar-nav navbar-right">
